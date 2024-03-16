4327cc2c27ce9b070d4065cd425c7753e9d56738
cucuruchi-project <156191099+cucuruchi-project@users.noreply.github.com>
2024-03-16 14:49:24 +0900

    Fix: Inventory 타입 확인 방법 변경

--- src/main/java/com/cucuruchi/banitem/listener/BanItemInventoryListner.java
+++ src/main/java/com/cucuruchi/banitem/listener/BanItemInventoryListner.java
@@ -1,9 +1,11 @@
 package com.cucuruchi.banitem.listener;
 
+import com.cucuruchi.banitem.inventory.BanitemInventory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
@@ -13,10 +15,12 @@ public class BanItemInventoryListner implements Listener {
     @EventHandler
     public void InventoryClickEvent(InventoryClickEvent event){
         Player player = (Player) event.getWhoClicked();
-        ItemStack currentItem = event.getCurrentItem();
-        if (currentItem == null) {
+        Inventory inv = event.getClickedInventory();
+        if (inv == null || !(inv.getHolder() instanceof BanitemInventory)){
             return;
         }
+        ItemStack currentItem = event.getCurrentItem();
         event.setCancelled(true);
+        if (currentItem == null) { return; }
     }
 }
--- src/main/java/com/cucuruchi/banitem/listener/BanItemSettingInventoryListener.java
+++ src/main/java/com/cucuruchi/banitem/listener/BanItemSettingInventoryListener.java
@@ -1,10 +1,13 @@
 package com.cucuruchi.banitem.listener;
 
+import com.cucuruchi.banitem.inventory.BanitemSettingInventory;
 import com.cucuruchi.harvillibrary.extension.ConfigExtension;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
@@ -21,13 +24,18 @@ public class BanItemSettingInventoryListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryCloseEvent(InventoryCloseEvent e){
-        Player player = (Player) e.getPlayer();
-        ItemStack[] itemType = e.getInventory().getContents();
-        int position = 0;
+    public void onInventoryCloseEvent(InventoryCloseEvent event){
+        Player player = (Player) event.getPlayer();
+        Inventory inv = event.getInventory();
+        if (inv == null || !(inv.getHolder() instanceof BanitemSettingInventory banitemSettingInventory)){
+            return;
+        }
+        ItemStack[] itemType = banitemSettingInventory.getInventory().getContents();
         for (ItemStack itemStack : itemType) {
-            banItems.clear();
-            banItems.add(itemStack.getType().name());
+            if (itemStack != null && !(itemStack.getType().isAir())){
+                banItems.clear();
+                banItems.add(String.valueOf(itemStack.getType()));
+            }
         }
         config.set("banitems", banItems.toArray());
         sendMessage(player, "밴 아이템 설정 완료");
